fc5ec9496f8533e1198e82658a8419fe97398eea
AndreySivaschenko <sivachenkoar@gmail.com>
2017-10-17 23:41:47 +0300

    add feedback

--- sumbit.php
+++ sumbit.php
@@ -1,17 +1,26 @@
 <?php
 	if(!empty($_POST['Name']) AND !empty($_POST['Email']) AND !empty($_POST['Message']){
 
-		$email = "sivaschenkoar@gmail.com";
+
+		$headers = 'From: Andrey Sivashchenko \r\n'.
+					'Reply-To:sivaschenkoar@gmail.com \r\n'.
+					'X-Mailer: PHP/'. phpversion();
+		$to = 'sivaschenkoar@gmail.com';
 		$theme = 'Новое сообщение';
 
 		$letter = "Данные сообщения:\r\n";
 		$letter .= 'Name:'.$_POST['Name'].'\r\n';
 		$letter .= 'Email:'.$_POST['Email'].'\r\n';
-		$letter .= 'Messages:'.$_POST['Messages'].'\r\n'
-		mail($email,$theme, $letter);
+		$letter .= 'Messages:'.$_POST['Messages'].'\r\n';
+		if(mail($email,$theme, $letter, $headers)){
+			header('Location:/thankyou.php');
+		}
+		else{
+			header('Location:/');
+		}
 	}
 	else{
-
+		header('Location:/');
 	}
 
 
